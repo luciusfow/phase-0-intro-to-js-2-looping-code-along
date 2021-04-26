6c7c7d0f2e229ce8ac8baa71cd89da1fbc213250
Lucius Fowler <luciusfow@gmail.com>
2021-04-26 19:36:38 -0400

    commit tree eebca78775cbd14ebc03c9d7984e446867f20242

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+// const person = ['Lisa', 'Kaitlin', 'Jan']
+// const suprise = "surprise"
+// function writeCards(person, surprise){
+//     let noteArray = []
+//     for (let i = 0; i < person.length; i++){
+//       let note = `Thank you, ${person[i]}, for the wonderful ${surprise} gift!`
+//       noteArray.push(surprise)
+//     } 
+//     return noteArray
+// }
+const person = [ 'Lisa', 'Kaitlin', 'Jan' ]
+const surprise = "surprise"
+function writeCards(person, surprise){
+    let messageArray = []
+    for (let i = 0; i < person.length; i++){
+     let message = `Thank you, ${person[i]}, for the wonderful ${surprise} gift!`
+        messageArray.push(message)
+    }
+    return messageArray
+}
+function countDown(num){
+    let numOG = num 
+    while (numOG >= 0){
+        console.log(numOG--)
+    }
+}
